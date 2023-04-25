5128074f19a56cfabab083cf78ebdd65bb7a80cc
Mark Fabian <markfabian@6.242.21.198.tigernet.wifi.dyn.clemson.edu>
2023-04-25 14:16:51 -0400

    adding buttons to GUI

--- GUI.py
+++ GUI.py
@@ -1,22 +1,30 @@
 import tkinter as tk
  
-root = tk.Tk()
- 
-root.title("Packet Sniffer")
-root.minsize(500,400)
-label = tk.Label(root, text = "Welcome to the python networking tool", font=('Arial',18))
-label.pack(padx=20,pady=20)
 
+class GUI:
+    def __init__(self):
 
+        self.root = tk.Tk()
+        self.root.title("Packet Sniffer")
+        self.root.minsize(500,400)
+        self.label = tk.Label(self.root, text = "Welcome to the python networking tool", font=('Arial',18))
+        self.label.pack(padx=20,pady=20)
 
+        self.buttonframe = tk.Frame(self.root)
+        self.buttonframe.columnconfigure(0,weight=1)
+        self.buttonframe.columnconfigure(1,weight=1)
+        # buttonframe.columnconfigure(2,weight=1)
+        btn1 = tk.Button(self.buttonframe,text="Start Sniffing",font=('Arial',18),command = self.start_sniffing)
+        btn2 = tk.Button(self.buttonframe,text = "End Sniffing",font=('Arial',18), command = self.stop_sniffing)
+        btn1.grid(row=0, column =0, sticky=tk.W+tk.E)
+        btn2.grid(row=0, column=1,sticky=tk.W+tk.E )
+        self.buttonframe.pack(fill='x')
 
-buttonframe = tk.Frame(root)
-buttonframe.columnconfigure(0,weight=1)
-buttonframe.columnconfigure(1,weight=1)
-# buttonframe.columnconfigure(2,weight=1)
-btn1 = tk.Button(buttonframe,text="Start Sniffing",font=('Arial',18))
-btn2 = tk.Button(buttonframe,text = "End Sniffing",font=('Arial',18))
-btn1.grid(row=0, column =0, sticky=tk.W+tk.E)
-btn2.grid(row=0, column=1,sticky=tk.W+tk.E )
-buttonframe.pack(fill='x')
-root.mainloop()
+    
+        self.root.mainloop()
+    def start_sniffing(self):
+        print("start sniffing")
+    def stop_sniffing(self):
+        print("stop sniffing")
+
+GUI()
