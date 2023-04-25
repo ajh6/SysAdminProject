ce3e405bbc797682d0a5c1a163b9ef69c93d9cb6
Mark Fabian <markfabian@6.242.21.198.tigernet.wifi.dyn.clemson.edu>
2023-04-25 12:57:56 -0400

    instructions

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ Install:
 Command: $ pip install scapy
 
 using Scapy in a python script requires the .py file to be ran in root privilage
+
+Link to documentation:
+https://scapy.readthedocs.io/en/latest/installation.html
--- workspace.py
+++ workspace.py
@@ -1,7 +1,7 @@
-
 #using Scapy in a python script requires the .py file to be ran in root privilage
 #run this .py file using: sudo python3 workspace.py
 
+#Right now this is just a basic 10 line packet sniff to show functionality
 
 from scapy.all import *
 
