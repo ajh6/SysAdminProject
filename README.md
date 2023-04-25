b4d453dd1a96ab7ee767128525e73c96628b3b03
Mark Fabian <markfabian@6.242.21.198.tigernet.wifi.dyn.clemson.edu>
2023-04-25 12:55:14 -0400

    ReadMe added

---
+++ README.md
@@ -0,0 +1,4 @@
+Install:
+Command: $ pip install scapy
+
+using Scapy in a python script requires the .py file to be ran in root privilage
--- workspace.py
+++ workspace.py
@@ -1,5 +1,8 @@
+
 #using Scapy in a python script requires the .py file to be ran in root privilage
 #run this .py file using: sudo python3 workspace.py
+
+
 from scapy.all import *
 
 a=sniff(count=10)
