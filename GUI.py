d5b6af987f907039c3e05c0c6f3d42631d74dfcc
ajh6 <ajhill882@gmail.com>
2023-05-01 19:51:55 -0400

    Added count function and trying to print to GUI

--- GUI.py
+++ GUI.py
@@ -1,5 +1,5 @@
-#using Scapy in a python script requires the .py file to be ran in root privilage
-#run this .py file using: sudo python3 filename
+# using Scapy in a python script requires the .py file to be ran in root privilage
+# run this .py file using: sudo python3 filename
 
 
 import tkinter as tk
@@ -10,61 +10,70 @@ import threading
 import collections
 import scapy.all as scapy
 
+from collections import Counter
+
 thread = None
 should_stop = False
 subdomain = ''
 source_ip_dict = collections.defaultdict(list)
+count = Counter()
 
 
 def start_sniffing():
-        print("start sniffing")
-        global should_stop
-        global thread
-        global subdomain
-        subdomain = subdomain_entry.get()
-        if (thread is None) or (not thread.is_alive()):
-            should_stop = False
-            thread = threading.Thread(target=sniffing).start()
-
+    print("start sniffing")
+    global should_stop
+    global thread
+    global subdomain
+    subdomain = subdomain_entry.get()
+    if (thread is None) or (not thread.is_alive()):
+        should_stop = False
+        thread = threading.Thread(target=sniffing).start()
 
 
 def handler(packet):
-        global source_ip_dict
-        global subdomain
-        treev
-        print(packet.show())
-        
+    global source_ip_dict
+    global subdomain
+    global count
+    treev
+    print(packet.show())
 
-        if 'IP' in packet:
-            src_ip = packet['IP'].src
-            dest_ip = packet['IP'].dst
+    if 'IP' in packet:
+        src_ip = packet['IP'].src
+        dest_ip = packet['IP'].dst
 
-            if src_ip[0:len(subdomain)] == subdomain:
-                if src_ip not in source_ip_dict:
-                    source_ip_dict[src_ip].append(dest_ip)
+        if src_ip[0:len(subdomain)] == subdomain:
+            if src_ip not in source_ip_dict:
+                source_ip_dict[src_ip].append(dest_ip)
 
-                    row = treev.insert('', index=tk.END, text=src_ip)
-                    treev.insert(row, tk.END, text=dest_ip)
-                    treev.pack(fill=tk.X)
-            else:
-                if dest_ip not in source_ip_dict[src_ip]:
-                    source_ip_dict[src_ip].append(dest_ip)
-                    cur_item = treev.focus()
-                    if (treev.item(cur_item)['text'] == src_ip):
-                        treev.insert(cur_item, tk.END, text=dest_ip)
+                row = treev.insert('', index=tk.END, text=src_ip)
+                treev.insert(row, tk.END, text=dest_ip)
+                treev.pack(fill=tk.X)
+        else:
+            if dest_ip not in source_ip_dict[src_ip]:
+                source_ip_dict[src_ip].append(dest_ip)
+                cur_item = treev.focus()
+                if (treev.item(cur_item)['text'] == src_ip):
+                    treev.insert(cur_item, tk.END, text=dest_ip)
+
+    # count calculation
+    for ip in source_ip_dict:
+        count[ip] += 1
+
 
 def sniffing():
-        scapy.sniff(prn=handler, stop_filter=stop_sniffing)
+    scapy.sniff(prn=handler, stop_filter=stop_sniffing)
+
 
 def stop_sniffing(packet):
-        global should_stop
-  
-        return should_stop
+    global should_stop
+
+    return should_stop
+
+
 def stop_button():
-     global should_stop
-     should_stop = True
-
-
+    global should_stop
+    global count
+    should_stop = True
 
 
 root = tk.Tk()
@@ -79,12 +88,19 @@ buttonframe.columnconfigure(0, weight=1)
 buttonframe.columnconfigure(1, weight=1)
 # buttonframe.columnconfigure(2,weight=1)
 btn1 = tk.Button(buttonframe, text="Start Sniffing",
-                      font=('Arial', 18), command=start_sniffing)
+                 font=('Arial', 18), command=start_sniffing)
 btn2 = tk.Button(buttonframe, text="End Sniffing",
-                       font=('Arial', 18), command=stop_button)
+                 font=('Arial', 18), command=stop_button)
 btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
 btn2.grid(row=0, column=1, sticky=tk.W+tk.E)
 buttonframe.pack(fill='x')
+
+
+# new textbox for count
+textbox = tk.Text(root, height=25, width=25)
+textbox.config(state='disabled')
+textbox.pack()
+
 
 subdomain_entry = tk.Entry(root)
 subdomain_entry.pack(ipady=5, ipadx=50, pady=10)
@@ -92,4 +108,3 @@ treev = ttk.Treeview(root, height=400)
 treev.column('#0')
 
 root.mainloop()
-
